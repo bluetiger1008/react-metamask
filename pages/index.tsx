58ed7e7bf666068b9ae616e525f831482a4ef9dd
bluetiger1008 <bluetiger.2611008@gmail.com>
2021-10-28 07:47:27 -0700

    Add material UI and remove unused codes

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,33 +4,20 @@ import {
   NoEthereumProviderError,
   UserRejectedRequestError as UserRejectedRequestErrorInjected,
 } from '@web3-react/injected-connector'
-import { UserRejectedRequestError as UserRejectedRequestErrorWalletConnect } from '@web3-react/walletconnect-connector'
-import { UserRejectedRequestError as UserRejectedRequestErrorFrame } from '@web3-react/frame-connector'
 import { Web3Provider } from '@ethersproject/providers'
 import { formatEther } from '@ethersproject/units'
+import { Container, Button, Box, CircularProgress } from '@material-ui/core'
+import CheckIcon from '@material-ui/icons/Check'
 
 import { useEagerConnect, useInactiveListener } from '../hooks'
 import { injected } from '../connectors'
-import { Spinner } from '../components/Spinner'
-
-enum ConnectorNames {
-  Injected = 'Injected',
-}
-
-const connectorsByName: { [connectorName in ConnectorNames]: any } = {
-  [ConnectorNames.Injected]: injected,
-}
 
 function getErrorMessage(error: Error) {
   if (error instanceof NoEthereumProviderError) {
     return 'No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.'
   } else if (error instanceof UnsupportedChainIdError) {
     return "You're connected to an unsupported network."
-  } else if (
-    error instanceof UserRejectedRequestErrorInjected ||
-    error instanceof UserRejectedRequestErrorWalletConnect ||
-    error instanceof UserRejectedRequestErrorFrame
-  ) {
+  } else if (error instanceof UserRejectedRequestErrorInjected) {
     return 'Please authorize this website to access your Ethereum account.'
   } else {
     console.error(error)
@@ -116,17 +103,11 @@ function Account() {
 
   return (
     <>
-      <span>Account</span>
+      <span>Address</span>
       <span role="img" aria-label="robot">
         🤖
       </span>
-      <span>
-        {account === null
-          ? '-'
-          : account
-          ? `${account.substring(0, 6)}...${account.substring(account.length - 4)}`
-          : ''}
-      </span>
+      <span>{account === null ? '-' : account ? `${account}` : ''}</span>
     </>
   )
 }
@@ -181,9 +162,7 @@ function Header() {
           display: 'grid',
           gridGap: '1rem',
           gridTemplateColumns: '1fr min-content 1fr',
-          maxWidth: '20rem',
           lineHeight: '2rem',
-          margin: 'auto',
         }}
       >
         <ChainId />
@@ -213,120 +192,48 @@ function App() {
   // handle logic to connect in reaction to certain events on the injected ethereum provider, if it exists
   useInactiveListener(!triedEager || !!activatingConnector)
 
-  return (
-    <>
-      <Header />
-      <hr style={{ margin: '2rem' }} />
-      <div
-        style={{
-          display: 'grid',
-          gridGap: '1rem',
-          gridTemplateColumns: '1fr 1fr',
-          maxWidth: '20rem',
-          margin: 'auto',
-        }}
-      >
-        {Object.keys(connectorsByName).map((name) => {
-          const currentConnector = connectorsByName[name]
-          const activating = currentConnector === activatingConnector
-          const connected = currentConnector === connector
-          const disabled = !triedEager || !!activatingConnector || connected || !!error
+  const currentConnector = injected
+  const activating = currentConnector === activatingConnector
+  const connected = currentConnector === connector
+  const disabled = !triedEager || !!activatingConnector || connected || !!error
 
-          return (
-            <button
-              style={{
-                height: '3rem',
-                borderRadius: '1rem',
-                borderColor: activating ? 'orange' : connected ? 'green' : 'unset',
-                cursor: disabled ? 'unset' : 'pointer',
-                position: 'relative',
-              }}
-              disabled={disabled}
-              key={name}
-              onClick={() => {
-                setActivatingConnector(currentConnector)
-                activate(connectorsByName[name])
-              }}
-            >
-              <div
-                style={{
-                  position: 'absolute',
-                  top: '0',
-                  left: '0',
-                  height: '100%',
-                  display: 'flex',
-                  alignItems: 'center',
-                  color: 'black',
-                  margin: '0 0 0 1rem',
-                }}
-              >
-                {activating && <Spinner color={'black'} style={{ height: '25%', marginLeft: '-1rem' }} />}
-                {connected && (
-                  <span role="img" aria-label="check">
-                    ✅
-                  </span>
-                )}
-              </div>
-              {name}
-            </button>
-          )
-        })}
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+  return (
+    <Container>
+      <Header />
+      <Box display="flex" alignItems="center">
+        <Button
+          variant="contained"
+          color="primary"
+          disabled={disabled}
+          onClick={() => {
+            setActivatingConnector(currentConnector)
+            activate(injected)
+          }}
+          startIcon={connected && <CheckIcon />}
+        >
+          {activating ? (
+            <>
+              <CircularProgress size={20} />
+              <span style={{ marginLeft: '.5rem' }}>Activating</span>
+            </>
+          ) : (
+            <>{connected ? 'Activated' : 'Activate'}</>
+          )}
+        </Button>
         {(active || error) && (
-          <button
-            style={{
-              height: '3rem',
-              marginTop: '2rem',
-              borderRadius: '1rem',
-              borderColor: 'red',
-              cursor: 'pointer',
-            }}
+          <Button
             onClick={() => {
               deactivate()
             }}
+            variant="contained"
+            color="secondary"
+            style={{ marginLeft: '1rem' }}
           >
             Deactivate
-          </button>
+          </Button>
         )}
-
-        {!!error && <h4 style={{ marginTop: '1rem', marginBottom: '0' }}>{getErrorMessage(error)}</h4>}
-      </div>
-
-      <hr style={{ margin: '2rem' }} />
-
-      <div
-        style={{
-          display: 'grid',
-          gridGap: '1rem',
-          gridTemplateColumns: 'fit-content',
-          maxWidth: '20rem',
-          margin: 'auto',
-        }}
-      >
-        {!!(library && account) && (
-          <button
-            style={{
-              height: '3rem',
-              borderRadius: '1rem',
-              cursor: 'pointer',
-            }}
-            onClick={() => {
-              library
-                .getSigner(account)
-                .signMessage('👋')
-                .then((signature: any) => {
-                  window.alert(`Success!\n\n${signature}`)
-                })
-                .catch((error: any) => {
-                  window.alert('Failure!' + (error && error.message ? `\n\n${error.message}` : ''))
-                })
-            }}
-          >
-            Sign Message
-          </button>
-        )}
-      </div>
-    </>
+      </Box>
+      <Box>{!!error && <h4 style={{ marginTop: '1rem', marginBottom: '0' }}>{getErrorMessage(error)}</h4>}</Box>
+    </Container>
   )
 }
